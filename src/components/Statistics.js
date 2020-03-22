7a5ecd26140298e5049a00335aa0d50d40c9f60e
Careena12345 <45653779+Careena12345@users.noreply.github.com>
2020-03-22 21:03:11 +0200

    add

---
+++ src/components/Statistics.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
+  <>
+    <h3>Statistics</h3>
+    {total > 0 && (
+      <ul>
+        <li>Good:{good}</li>
+        <li>Neutral:{neutral}</li>
+        <li>Bad:{bad}</li>
+        <li>Total:{total}</li>
+        <li>Positive Feedback:{good && positivePercentage}</li>
+      </ul>
+    )}
+  </>
+);
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.string.isRequired
+};
+
+export default Statistics;
